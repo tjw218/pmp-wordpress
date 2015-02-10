5942596356460d258e1b04a32eb8e96b0b4f0d1d
Ryan Nagle <rmnagle@gmail.com>
2015-02-10 16:29:38 -0600

    rework admin menus, add sub menu pages for settings and search

--- plugin.php
+++ plugin.php
@@ -34,13 +34,38 @@ add_action('widgets_init', 'pmp_init');
  * @since 0.1
  */
 function pmp_plugin_menu() {
-	$page_title = 'PMP Settings';
-	$menu_title = 'PMP Settings';
+	$page_title = 'Public Media Platform';
+	$menu_title = 'Public Media Platform';
 	$capability = 'manage_options';
 	$menu_slug = 'pmp-options-menu';
 	$function = 'pmp_options_page';
 
 	add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function);
+
+	$sub_menus = array(
+		array(
+			'page_title' => 'Settings',
+			'menu_title' => 'Settings',
+			'capability' => 'manage_options',
+			'menu_slug' => 'pmp-options-menu',
+			'function' => 'pmp_options_page'
+		),
+		array(
+			'page_title' => 'Search',
+			'menu_title' => 'Search',
+			'capability' => 'edit_posts',
+			'menu_slug' => 'pmp-search',
+			'function' => 'pmp_search_page'
+		)
+	);
+
+	foreach ($sub_menus as $sub_menu) {
+		add_submenu_page(
+			'pmp-options-menu', $sub_menu['page_title'], $sub_menu['menu_title'],
+			$sub_menu['capability'], $sub_menu['menu_slug'], $sub_menu['function']
+		);
+	}
+
 }
 add_action('admin_menu', 'pmp_plugin_menu');
 
@@ -54,4 +79,16 @@ function pmp_options_page() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 
 	pmp_render_template('settings.php');
+}
+
+/**
+ * Render the plugin's search page
+ *
+ * @since 0.1
+ */
+function pmp_search_page() {
+	if (!current_user_can('edit_posts'))
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+
+	// TODO: Render the search page
 }
