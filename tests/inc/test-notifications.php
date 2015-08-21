95a370e9f6ad5d3f05234dd8c2d53c2e4daef268
Ryan Nagle <rmnagle@gmail.com>
2015-08-21 14:09:18 -0500

    stubbed out tests for notifications rig

---
+++ tests/inc/test-notifications.php
@@ -0,0 +1,41 @@
+<?php
+
+class TestNotifications extends WP_UnitTestCase {
+
+	function test_pmp_bless_notification_query_var() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_notifications_template_redirect() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_send_subscription_request() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_post_subscription_data() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_store_verification_token() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_get_verification_token() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_subscription_verification() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_get_topic_urls() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pmp_do_notification_callback() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+}
